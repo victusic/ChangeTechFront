dec0d6fba30f6906194ef5185dc3700fb29d9c33
Victor <horosivv@gmail.com>
2025-04-20 20:28:36 +0500

    CT-12 try to fix svg

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,32 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {};
+/** @type {NextConfig} */
+const nextConfig: NextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [/url/] },
+          use: ['@svgr/webpack'],
+        },
+      );
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
+};
 
 export default withNextIntl(nextConfig);
-
--- src/assets/icons/links/index.ts
+++ src/assets/icons/links/index.ts
@@ -1,4 +1,4 @@
 export { CvIcon } from './CvIcon';
 export { LinkedInIcon } from './LinkedInIcon';
-export { GitIcon } from './GitIcon';
+//export { GitIcon } from './GitIcon';
 export { TelegramIcon } from './TelegramIcon';
